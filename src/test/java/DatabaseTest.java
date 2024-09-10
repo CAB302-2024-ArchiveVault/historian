89a4e90f0d46bd5b1568abbbc46aac3615c8a29a
Isaac Shea <isaac_shea@icloud.com>
2024-09-10 21:29:15 +1000

    Added CI dependencies for testing

---
+++ src/test/java/DatabaseTest.java
@@ -0,0 +1,15 @@
+
+import com.example.historian.utils.SqliteConnection;
+import org.junit.jupiter.api.*;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DatabaseTest {
+  @Test
+  public void testConnection() {
+    Connection conn = SqliteConnection.getInstance();
+    assertEquals(true, conn != null);
+  }
+}
